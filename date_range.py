5ae8661858c5de28d5c5ee43740a7d14d839f7a5
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2022-02-07 21:31:39 -0300

    document code

--- date_range.py
+++ date_range.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 def date_range(dates):
     sorted_date = sorted(dates)
-    selected_start_date = st.sidebar.selectbox('Start date', sorted_date)
-    selected_end_date = st.sidebar.selectbox('End date', sorted_date)
+    # get min and max values' indexes on avaliable dates """
+    max_data_index = sorted_date.index(max(sorted_date))
+    min_data_index = sorted_date.index(min(sorted_date))
+
+    selected_start_date = st.sidebar.selectbox('Start date', sorted_date, index=min_data_index)
+    selected_end_date = st.sidebar.selectbox('End date', sorted_date, index=max_data_index)
     return selected_start_date, selected_end_date
 
--- main.py
+++ main.py
@@ -1,51 +1,61 @@
+
 import pandas as pd
 import streamlit as st
 import base64
 from clean_df import clean_df
-""" filters import """
+# import filters
 from date_range import date_range
 from mag_filter import mag_filter
+# general functions
+from filedownload import filedownload
 
 st.title('Earhquake data')
-
+# read file
 file = pd.read_csv('significant_month.csv')
 file_df = pd.DataFrame(file)
 
 def select_data(df):
+    # clean time data => convert to YYYY-MM-DD date format
     df = clean_df(df)
+    # select desired columns from dataframe
     columns = ['time', 'longitude', 'depth', 'mag', 'magSource', 'locationSource']
     new_df = df[columns]
     return new_df
 
-selected_data = select_data(file_df)
-""" FILTER PARAMETERS """
 
+
+selected_data = select_data(file_df)
+## FILTER PARAMETERS """
+# magnitude source filter
 mags = selected_data.magSource.unique()
 selected_source = mag_filter(mags)
 
 
-st.sidebar.title("Date range")
+# date range filter
 dates = selected_data["time"].unique()
 selected_start_date, selected_end_date = date_range(dates)
 
+# number of rows filter
+num_of_earthquakes = st.sidebar.slider(
+    'Number of earthquakes', 1, 7, value=len(selected_data.index)
+)
 
-print('source s', selected_source)
+## DATA SELECTION """
+# selected data based on filters data
 new_selected_data = selected_data[(selected_data.magSource.isin(selected_source))
 & (selected_data['time'] > selected_start_date)
 & (selected_data['time'] < selected_end_date)]
 
-num_of_earthquakes = st.sidebar.slider(
-    'Number of earthquakes', 1, 7, value=len(new_selected_data.index)
-)
+# limit rows based on number of earthquakes data filter
 new_selected_data = new_selected_data[:num_of_earthquakes]
 
+#plot dataframe
 st.dataframe(new_selected_data)
 
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
-    return href
+
+# get download href
+href = filedownload(new_selected_data)
 
 
-st.markdown(filedownload(new_selected_data), unsafe_allow_html=True)
+## DOWNLOAD FILE"""
+st.markdown(href, unsafe_allow_html=True)
